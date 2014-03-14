800526a400348c1d50c246578ef67263b1313ff0
Bin He <binhe22@gmail.com>
2014-03-14 17:03:25 +0800

    add comment

--- test.py
+++
@@ -1,9 +0,0 @@
-import pexpect
-
-def test():
-    child=pexpect.spawn('ssh %s "%s"' %("root@115.156.219.157", "nohup ls&"))
-    a = child.expect("\].*(\d)")
-    print a
-    child.close()
-
-test()
